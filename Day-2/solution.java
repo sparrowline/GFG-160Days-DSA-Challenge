7017d48b1a4c607ef65758edb77cffc9b603f598
Syed Rizwan <syedrizwansat95@gmail.com>
2025-04-17 23:03:09 +0530

    add day 1 and 2 solution

---
+++ Day-2/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    void pushZerosToEnd(int[] arr) {
+        int j = 0;
+        for (int i = 0; i < arr.length; i++) {
+             if (arr[i] != 0) {
+                int temp = arr[i];
+                 arr[i] = arr[j];
+                  arr[j] = temp;
+                 j++;
+             }
+        }
+    }
+}
