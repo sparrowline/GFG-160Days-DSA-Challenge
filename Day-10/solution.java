31f346fa2b549d6e152eab467abff542f5b76a3f
Syed Rizwan <syedrizwansat95@gmail.com>
2025-04-22 13:08:00 +0530

    Implement Day 10 problem solution

---
+++ Day-10/solution.java
@@ -0,0 +1,15 @@
+class Kadan {
+    int maxSubarraySum(int[] arr) {
+        // Your code here
+        int maxSum = arr[0];
+        int currentSum = arr[0];
+        
+        for(int i=1;i<arr.length;i++){
+            
+            currentSum = Math.max(arr[i],currentSum+arr[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+        
+        return maxSum;
+    }
+}
