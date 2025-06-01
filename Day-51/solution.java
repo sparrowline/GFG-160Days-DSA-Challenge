a81b6b75beac161440a44c609ed8915ec0f9a93e
Syed Rizwan <syedrizwansat95@gmail.com>
2025-06-01 23:32:30 +0530

    add day 51 solution;

---
+++ Day-51/solution.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+class CountPairs {
+    int countPairs(int arr[], int target) {
+        // Your code here
+        Arrays.sort(arr);
+        int n = arr.length;
+        int left =0;
+        int right=n-1;
+        int count=0;
+        while(left<right){
+            if(arr[left]+arr[right]<target){
+                    count += (right-left);
+                    left++;
+                }else{
+                    right--;
+                }
+        }
+        return count;
+    }
+}
