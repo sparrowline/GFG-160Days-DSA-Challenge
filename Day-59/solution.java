171bf6d1da012a3c4065a46bd6dfd42a10ad06b7
Syed Rizwan <syedrizwansat95@gmail.com>
2025-06-09 22:38:41 +0530

    add day 59 solution;

---
+++ Day-59/solution.java
@@ -0,0 +1,27 @@
+// User function Template for Java
+
+class MaxWater {
+
+    public int maxWater(int arr[]) {
+        // Code Here
+        int maxArea = 0;
+        int left = 0;
+        int right = arr.length-1;
+        
+        while(left <right){
+            
+            int height = Math.min(arr[left],arr[right]);
+            int width = right-left;
+            int area = height * width;
+            
+            maxArea = Math.max(maxArea, area);
+            
+            if(arr[left]<arr[right]){
+                left++;
+            }else{
+                right--;
+            }
+        }
+        return maxArea;
+    }
+}
