60598b61635aa22d7db29f9fca446282385108f9
Syed Rizwan <syedrizwansat95@gmail.com>
2025-06-29 22:57:21 +0530

    add day 77 solution

---
+++ Day-77/solution.java
@@ -0,0 +1,30 @@
+//A Binary Tree node
+class Node1
+{
+    int data;
+    Node1 left, right;
+   Node1(int item)
+   {
+        data = item;
+        left = right = null;
+    }
+} 
+
+class Solution {
+    // Function to convert a binary tree into its mirror tree.
+    void mirror(Node1 node) {
+        // Your code here
+        
+        if(node == null) return;
+            
+            //swap left and right
+            Node1 temp = node.left;
+            node.left = node.right;
+            node.right = temp;
+            
+            //recur
+            mirror(node.left);
+            mirror(node.right);
+        
+    }
+}
